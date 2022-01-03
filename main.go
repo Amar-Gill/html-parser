ba5e2d0267f2f73955e0160171685f20762bf5ac
Amar Gill <amardeep@clearbanc.com>
2022-01-02 20:12:07 -0500

    initial commit. href parsing working. text parsing WIP

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+type Link struct {
+	Href string
+	Text string
+}
+
+func main() {
+
+	file, err := os.Open("ex1.html")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	doc, err := html.Parse(file)
+	if err != nil {
+		panic(err)
+	}
+
+	links := make([]Link, 0)
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		var link Link
+		hrefString := ""
+		text := ""
+
+		if n.Type == html.ElementNode && n.Data == "a" {
+			hrefString = parseHref(n)
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			if c.Type == html.TextNode {
+				text += c.Data
+			}
+			f(c)
+		}
+
+		link = Link{hrefString, text}
+		links = append(links, link)
+	}
+	f(doc)
+
+	fmt.Printf("%v", links)
+}
+
+func parseHref(n *html.Node) string {
+	var v string
+	for _, att := range n.Attr {
+		if att.Key == "href" {
+			v = att.Val
+		}
+	}
+	return v
+}
+
+// func parseText(n *html.Node, s string) string {
+
+// 	for c := n.FirstChild; c != nil; c = c.NextSibling {
+// 		if c.Type == html.TextNode {
+// 			s += c.Data
+// 		}
+// 		s += parseText(c, s)
+// 	}
+// 	return s
+// }
