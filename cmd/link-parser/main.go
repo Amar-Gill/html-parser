893b58e6f96af42e05bd91dd744cc7b5d26153d1
Amar Gill <amargill@Amars-MacBook-Air.local>
2022-04-30 00:26:37 -0400

    updated main file to use link-parser package

--- cmd/link-parser/main.go
+++ cmd/link-parser/main.go
@@ -5,13 +5,8 @@ import (
 	"fmt"
 	"os"
 
-	"golang.org/x/net/html"
+	"github.com/amar-gill/link-parser"
 )
-
-type Link struct {
-	Href string
-	Text string
-}
 
 func main() {
 	filename := flag.String("file", "ex1.html", "html file name")
@@ -21,53 +16,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
+	defer file.Close()	
 
-	doc, err := html.Parse(file)
+	links, err := linkparser.ParseLinks(file)
 	if err != nil {
 		panic(err)
 	}
 
-	links := make([]Link, 0)
-
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-
-		if n.Type == html.ElementNode && n.Data == "a" {
-			hrefString := parseHref(n)
-			text := parseText(n, "")
-			link := Link{hrefString, text}
-			links = append(links, link)
-		}
-
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
-	}
-
-	f(doc)
-
-	fmt.Printf("%v", links)
-}
-
-func parseHref(n *html.Node) string {
-	var v string
-	for _, att := range n.Attr {
-		if att.Key == "href" {
-			v = att.Val
-			break
-		}
-	}
-	return v
-}
-
-func parseText(n *html.Node, s string) string {
-
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		if c.Type == html.TextNode {
-			s += c.Data
-		}
-		s = parseText(c, s)
-	}
-	return s
+	fmt.Println(links)
 }
